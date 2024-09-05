e4981831b22a25ba6eb3fc3d085d9867faaf2a69
powerwolf1 <zaporojan2017@ya.ru>
2024-09-05 12:49:38 +0300

    adding creation of new cleared document

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import csv
 def main():
     duplicates = []
 
+    # Remove duplicates
     with open("username-or-email.csv", newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        header = tuple(next(reader))
         next(reader, None)
         seen_row = set()
 
@@ -17,7 +19,11 @@ def main():
             else:
                 seen_row.add(row_tuple)
 
-    print(duplicates)
+    # Create new cleared document without duplicates
+    data = list(seen_row)
+    data.insert(0, header)
+    writer = csv.writer(open("username-or-email-cleared.csv", "w", newline=''))
+    writer.writerows(data)
 
 
 if __name__ == '__main__':
